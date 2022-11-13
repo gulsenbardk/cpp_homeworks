e76ea06ec6abb30a654e30933759cdd01e4915f4
Gülşen Bardak <gulsenbardak1@gmail.com>
2022-11-13 11:38:47 +0100

    1.2

--- homework_1/task_2/build.sh
+++ homework_1/task_2/build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash 
 
 mkdir build 
-c++ -c  -I../include src/sum.cpp -o build/sum.o
-c++ -c  -I../include src/subtract.cpp -o build/subtract.o
-ar rcs build/libipb_arithmetic.a build/subtract.o build/sum.o
-
-mkdir -p results/bin
-mkdir -p results/lib
-cp build/libipb_arithmetic.a results/lib
+cd build
+c++ -c  -I../include ../src/main.cpp -o main.o
+c++ -c  -I../include ../src/subtract.cpp -o subtract.o
+c++ -c  -I../include ../src/sum.cpp -o sum.o
+ar rcs libipb_arithmetic.a sum.o subtract.o
+cd ..
+cp libipb_arithmetic.a results/lib
 
 c++ -std=c++17 -c -I include/ src/main.cpp -o build/main.o
-c++ -std=c++17 ../build/main.o -L . -lipb_arithmetic -o main
+c++ -std=c++17 build/main.o -L build/ -lipb_arithmetic -o results/bin/test_ipb_arithmetic
