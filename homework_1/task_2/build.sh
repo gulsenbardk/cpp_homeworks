450665507399200acdf920f84b1fcc3bea18c35d
Gülşen Bardak <gulsenbardak1@gmail.com>
2022-11-13 15:55:03 +0100

    1.10.1

--- homework_1/task_2/build.sh
+++ homework_1/task_2/build.sh
@@ -10,5 +10,3 @@ mkdir -p results/bin
 mkdir -p results/lib
 cp build/libipb_arithmetic.a ./results/lib
 
-c++ -std=c++17 -c -I include/ src/main.cpp -o build/main.o
-c++ -std=c++17 build/main.o -L results/lib/ -lipb_arithmetic -o results/bin/test_ipb_arithmetic
