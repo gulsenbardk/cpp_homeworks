730e5d0fd543edbacc91b3f0e943bff148ebf7fe
Gülşen Bardak <gulsenbardak1@gmail.com>
2022-11-21 21:28:28 +0100

    2.5

--- homework_2/task_1/task_1.cpp
+++ homework_2/task_1/task_1.cpp
@@ -13,32 +13,29 @@ int main() {
     // ------------------------------------------------- //
     // --------------------------------------------------------------------------------------- //
     int guessed_number = 0;
-    while (target_number != guessed_number) {
+    do {
         cin >> guessed_number;
-        cout << "Target number was: " << target_number << endl;
-
         if (cin.fail()) {
             cerr << "Error encountered, exiting..." << endl << target_number << endl;
             return EXIT_FAILURE;
         }
-        if ((target_number < 0) || (target_number > 99)) {
+        if (guessed_number > 0 && guessed_number < 99) {
+            if (guessed_number == target_number) {
+                cout << "Congratulations!!" << target_number << endl;
+            } else if (guessed_number < target_number) {
+                cout << "Your guessed number " << guessed_number
+                     << " is smaller than target number " << endl;
+            } else if (guessed_number > target_number) {
+                cout << "Your guessed number " << guessed_number << " is larger then target number "
+                     << endl;
+            }
+        } else if (0 > guessed_number || guessed_number > 99) {
             cerr << "[WARNING] : Number must be between 0 and 99" << endl;
-        } else if (guessed_number < target_number) {
-            cout << "Your guessed number is smaller than target number"
-                    "target number "
-                 << endl
-                 << target_number << endl;
-        } else if (guessed_number > target_number) {
-            cout << " Your guessed number is smaller than target number"
-                    "target number "
-                 << endl
-                 << target_number << endl;
-        } else if (guessed_number == target_number) {
-            cerr << "Congratulations!!!! " << endl << target_number << endl;
         } else {
             cerr << "ERROR" << endl;
         }
-    }
-    // --------------------------------------------------------------------------------------- //
+    } while (target_number != guessed_number);
+    // ---------------------------------------------------------------------------------------
+    // //
     return EXIT_SUCCESS;
 }
